0f0db02b7df9945e89060e45b307c9a5f71ae266
Javier J. Martínez M. <jj_dev@outlook.com>
2026-08-18 21:10:25 -0600

    feat: add search form to filter blogs by title, ex 6
    + Add search form to blogs list to filter items by title & update the url with search params.

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -1,18 +1,34 @@
+import Form from "next/form";
 import Link from "next/link";
 import { getBlogs } from "../services/blogs";
 
-const Blogs = () => {
-  const blogs = getBlogs();
+const Blogs = async ({
+  searchParams,
+}: {
+  searchParams: Promise<{ filter?: string }>;
+}) => {
+  const { filter } = await searchParams;
+  const searchTitle = filter && filter !== "";
+  const allBlogs = getBlogs();
+  const blogs = searchTitle
+    ? allBlogs.filter((blog) => blog.title.includes(filter))
+    : allBlogs;
 
   return (
     <div>
       <h2>Blogs</h2>
+      <Form action="">
+        <input name="filter" />
+        <button type="submit">Search</button>
+      </Form>
       <ul>
-        {blogs.sort(((a, b) => b.likes - a.likes)).map((blog) => (
-          <li key={blog.id}>
-            <Link href={`/blogs/${blog.id}`}>{blog.title}</Link>
-          </li>
-        ))}
+        {blogs
+          .sort((a, b) => b.likes - a.likes)
+          .map((blog) => (
+            <li key={blog.id}>
+              <Link href={`/blogs/${blog.id}`}>{blog.title}</Link>
+            </li>
+          ))}
       </ul>
     </div>
   );
