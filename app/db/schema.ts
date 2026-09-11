492bf5f89fbaf8f2ce2ab0fd3005b656d0a5e478
Javier J. Martínez M. <jj_dev@outlook.com>
2026-09-10 20:22:50 -0600

    feat: add password field for users table migration

--- app/db/schema.ts
+++ app/db/schema.ts
@@ -6,11 +6,14 @@ export const blogs = pgTable("blogs", {
   author: varchar({ length: 100 }).notNull(),
   url: varchar({ length: 500 }).notNull().unique(),
   likes: integer().notNull().default(0),
-  userId: integer("user_id").notNull().references(() => users.id),
+  userId: integer("user_id")
+    .notNull()
+    .references(() => users.id),
 });
 
 export const users = pgTable("users", {
   id: serial().primaryKey(),
   username: varchar({ length: 60 }).notNull(),
   name: varchar({ length: 255 }).notNull(),
+  passwordHash: varchar("password_hash", { length: 100 }).notNull().default(""),
 });
